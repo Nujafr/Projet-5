301ca3a3904241ecd7c6f6c493e51f3519dc2677
Nujafr <nujapro@gmail.com>
2024-09-12 18:34:24 +0200

    Feat: Add gallery from back-end with JS

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(data => {
+        const gallery = document.querySelector('.gallery');
+        data.forEach(work => {
+            const figure = document.createElement('figure');
+            const img = document.createElement('img');
+            img.src = work.imageUrl;
+            img.alt = work.title;
+            const figcaption = document.createElement('figcaption');
+            figcaption.textContent = work.title;
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+            gallery.appendChild(figure);
+        });
+    })
+    .catch(error => {
+        console.error('Erreur:', error);
+    })
