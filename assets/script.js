81a68fdea5facd3185a1e1bf8e07e2938591218e
Nujafr <nujapro@gmail.com>
2024-09-12 19:28:56 +0200

    feat: Add filters buttons

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,10 @@ fetch("http://localhost:5678/api/works")
     .then(response => response.json())
     .then(data => {
         const gallery = document.querySelector('.gallery');
-        data.forEach(work => {
+        const filter = document.querySelector('.filter');
+        
+        // Fonction pour créer un élément figure avec image et légende
+        function createFigure(work) {
             const figure = document.createElement('figure');
             const img = document.createElement('img');
             img.src = work.imageUrl;
@@ -11,7 +14,49 @@ fetch("http://localhost:5678/api/works")
             figcaption.textContent = work.title;
             figure.appendChild(img);
             figure.appendChild(figcaption);
-            gallery.appendChild(figure);
+            return figure;
+        }
+
+        data.forEach(work => {
+            gallery.appendChild(createFigure(work)); // Utiliser la fonction pour ajouter les éléments
+        });
+
+        // Créer le bouton "Tous"
+        const allButton = document.createElement('button');
+        allButton.textContent = 'Tous';
+        
+        // Ajouter un gestionnaire d'événements pour le clic sur le bouton "Tous"
+        allButton.addEventListener('click', () => {
+            gallery.innerHTML = ''; // Vider la galerie
+            data.forEach(work => {
+                gallery.appendChild(createFigure(work)); // Utiliser la fonction
+            });
+        });
+
+        filter.appendChild(allButton); // Ajouter le bouton "Tous" au filtre
+
+        const categories = new Set(); // Créer un ensemble pour les catégories uniques
+
+        data.forEach(work => {
+            categories.add(work.category.name); // Ajouter chaque catégorie à l'ensemble
+        });
+
+        categories.forEach(category => {
+            const button = document.createElement('button');
+            button.name = category;
+            button.textContent = category; // Ajouter le texte du bouton
+
+            // Ajouter un gestionnaire d'événements pour le clic
+            button.addEventListener('click', () => {
+                // Filtrer les objets en fonction de la catégorie
+                const filteredWorks = data.filter(work => work.category.name === category);
+                gallery.innerHTML = ''; // Vider la galerie avant d'ajouter les objets filtrés
+                filteredWorks.forEach(work => {
+                    gallery.appendChild(createFigure(work)); // Utiliser la fonction
+                });
+            });
+
+            filter.appendChild(button);
         });
     })
     .catch(error => {
